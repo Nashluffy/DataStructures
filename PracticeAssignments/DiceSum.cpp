5bcacb237f31ad1579095a26329e0fe36468e956
Nash Luffman <nashluffman@gmail.com>
2019-06-07 15:20:58 -0400

    Skeleton

---
+++ PracticeAssignments/DiceSum.cpp
@@ -0,0 +1,11 @@
+//Use recursive backtracking and pruning to print the combination of n dice faces that sums to an input number m
+
+int printDice(int faces, int sum){
+
+
+}
+
+int main(){
+	printDice(2,7);
+	printDice(3,12);
+}
