91596bad8d189e05ac73a0b65c542310c8dafaaf
Nash Luffman <Nash.Luffman@qorvo.com>
2019-06-20 16:54:55 -0400

    Pseudocode and first attempt

--- Assignment5/heap.cpp
+++ Assignment5/heap.cpp
@@ -1,6 +1,38 @@
+
+//Given: A sequence of numbers x1, x2, ..., xn input one-by-one, find the median as the numbers arrive.
+//See Heap lecture - 1 for solution. Solution implemented using a min-heap and max-heap that holds the bottom half and top half of the numbers respectively.
+
+//Heaps are implemented using the priority_queue container from the STL
+
+//The test function is provided below. Also included is a function print_queue if you need to view the contents of the heaps at any point.
+
+//Statistical median is the middle number of a group of numbers that have been arranged in order by size.
+//If there is an even number of terms, the median is the average of the two middle names
+
+//Pseudocode
+//Median is the max of min heap and max
+// 1 2 3 4 5 6 7 8 9 10
+// 5 + 6 / 2  = = 5.5
+//Lower half stored in minHeap -> 1 2 3 4 5
+//Upper half stored in maxHeap -> 6 7 8 9 10
+//Median would be minHeap.top() + maxHeap.min() / 2
+
+//Insert a new element, 11, and calculate the Median
+//If minHeap.top() <= newElement
+    // minHeap.insert(newElement)
+//If maxHeap.top() >= newElement
+    //maxHeap.insert(newElement)
+//If (minHeap.size()%2 == 0)
+    //return median = minHeap.top()
+//Else If (maxHeap.size()%2 == 0)
+    //return median = maxHeap.top()
+//Else
+    //return median = minHeap.top() + maxHeap.top() / 2
+
 #include <iostream>
 #include <queue>
 #include <vector>
+#include <time.h>
 using namespace std;
 
 const int MAX_VAL = 100;
@@ -32,12 +64,24 @@ void print_queue(T& q) {
     }
     std::cout << '\n';
 }
- 
+
 
 int find_median(priority_queue<int, vector<int>, greater<int>>& h_high, priority_queue<int>& h_low, int num) {
-
- // Your code here...
-
- 
+int median;
+if (num <= h_low.top()){
+      h_low.push(num);
+}
+else{
+  h_high.push(num);
+}
+if (h_low.size()%2 != 0){
+  return median = h_low.top();
+}
+else if (h_high.size()%2 != 0){
+  return median = h_high.top();
+}
+else{
+  return median = h_low.top() + h_high.top() / 2 ;
+}
 
 }
